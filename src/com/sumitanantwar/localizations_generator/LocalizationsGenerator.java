ec42c78c8351a548123083376d5b87e59fcc5b96
Sumit <sumit.anantwar@gmail.com>
2020-10-29 16:13:11 +0200

    v22

--- src/com/sumitanantwar/localizations_generator/LocalizationsGenerator.java
+++ src/com/sumitanantwar/localizations_generator/LocalizationsGenerator.java
@@ -45,7 +45,7 @@ public class LocalizationsGenerator {
         {
             File baseFolder = new File("Localization");
 
-            FileInputStream file = new FileInputStream(new File(baseFolder.getPath() + File.separator + "PopGuide_Translations_V20.xlsx"));
+            FileInputStream file = new FileInputStream(new File(baseFolder.getPath() + File.separator + "PopGuide_Translations.xlsx"));
 
             //Create Workbook instance holding reference to .xlsx file
             XSSFWorkbook workbook = new XSSFWorkbook(file);
