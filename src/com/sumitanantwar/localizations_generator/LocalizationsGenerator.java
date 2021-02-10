639a3cc689e4c4c8f0ab02afaa13bb8f27b19c68
Sumit <sumit.anantwar@gmail.com>
2021-02-11 01:17:29 +0200

    VoxConnect V22

--- src/com/sumitanantwar/localizations_generator/LocalizationsGenerator.java
+++ src/com/sumitanantwar/localizations_generator/LocalizationsGenerator.java
@@ -45,7 +45,7 @@ public class LocalizationsGenerator {
         {
             File baseFolder = new File("Localization");
 
-            FileInputStream file = new FileInputStream(new File(baseFolder.getPath() + File.separator + "VoxConnect_Translations_V21.xlsx"));
+            FileInputStream file = new FileInputStream(new File(baseFolder.getPath() + File.separator + "VoxConnect_Translations_V22.xlsx"));
 
             //Create Workbook instance holding reference to .xlsx file
             XSSFWorkbook workbook = new XSSFWorkbook(file);
